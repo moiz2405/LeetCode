51c6388d95f6f1963172f089a7303702d8c77267
moiz2405 <km856307@gmail.com>
2024-08-05 22:12:42 +0530

    Time: 2144 ms (32.89%), Space: 12.5 MB (38.39%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for x in range(len(nums)):
+            for y in range(x+1,len(nums)):
+                if nums[x]+nums[y]==target:
+                    return[x,y]
+        return []            
+        
+
+
+
+
+
+      
+        
