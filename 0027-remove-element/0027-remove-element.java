7ba4f4b7aa81f220be6aeda2000b16e6ddd2dffd
MoizCodes <km856307@gmail.com>
2024-08-07 23:32:59 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (15.32%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i,j=0;
+        for(i=0;i<nums.length;i++){
+             if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;  
+             }
+             
+        }
+        return j;
+    }
+    
+}
