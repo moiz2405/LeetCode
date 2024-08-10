8f079441ce8d42b94bc06924398288ea07fc0a36
MoizCodes <km856307@gmail.com>
2024-08-11 00:19:01 +0530

    Time: 1 ms (86.01%), Space: 40.8 MB (47.70%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {    
+        if (x == 0 || x == 1)
+            return x;
+        int start = 1,end = x,mid = -1;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if ((long) mid * mid > (long) x)
+                end = mid - 1;
+            else if (mid * mid == x)      
+                return mid;
+            else
+                start = mid + 1;
+        }
+        return end;
+    }
+}
