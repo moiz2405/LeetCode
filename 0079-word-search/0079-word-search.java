e03960110882ea6592e029b0b919cfa17b29530f
MoizCodes <km856307@gmail.com>
2024-09-16 13:05:49 +0530

    Time: 133 ms (81.68%), Space: 41.7 MB (32.19%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || word.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            } }
+        return false; 
+    }
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '#';  
+
+        boolean found = dfs(board, word, i + 1, j, index + 1) ||
+                        dfs(board, word, i - 1, j, index + 1) ||
+                        dfs(board, word, i, j + 1, index + 1) ||
+                        dfs(board, word, i, j - 1, index + 1);
+        board[i][j] = temp;
+
+        return found;
+    }
+}
