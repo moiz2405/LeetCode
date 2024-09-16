ce61e5da0e251400d46a9e5d20649d45dd7594da
MoizCodes <km856307@gmail.com>
2024-09-16 12:47:13 +0530

    Time: 1 ms (24.29%), Space: 41.6 MB (91.35%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public void sortColors(int[] nums) {
+        sort(nums);
+    }
+
+    void sort(int[] arr) {
+        Arrays.sort(arr);  
+    }
+}
