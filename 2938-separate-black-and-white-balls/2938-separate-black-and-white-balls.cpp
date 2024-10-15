a24a35eadaee01345396fe97f11703113465c842
MoizCodes <km856307@gmail.com>
2024-10-15 23:25:31 +0530

    Time: 31 ms (73.47%), Space: 15.5 MB (76.42%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -4,15 +4,11 @@ public:
         int whitePosition = 0;
         long long totalSwaps = 0;
 
-        // Iterate through each ball (character) in the string
         for (int currentPos = 0; currentPos < s.length(); currentPos++) {
             if (s[currentPos] == '0') {
-                // Calculate the number of swaps needed
-                // to move it to the leftmost available position
+
                 totalSwaps += currentPos - whitePosition;
 
-                // Move the next available position for a white ball one step to
-                // the right
                 whitePosition++;
             }
         }
