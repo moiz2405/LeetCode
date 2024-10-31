b9a4c716d5456ab8b5f060cfbd4de68186d9e577
MoizCodes <km856307@gmail.com>
2024-11-01 00:45:17 +0530

    Time: 188 ms (34.13%), Space: 146.1 MB (74.14%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(std::vector<int>& nums, int indexDiff, int valueDiff) {
+        std::set<long> window; 
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > indexDiff) {
+                window.erase(nums[i - indexDiff - 1]); 
+            }
+            auto pos = window.lower_bound((long)nums[i] - valueDiff);
+     
+            if (pos != window.end() && *pos <= (long)nums[i] + valueDiff) {
+                return true; // If found, return true
+            }
+            window.insert(nums[i]); 
+        }
+        return false; 
+    }
+};
