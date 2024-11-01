3b43ff4e4f95a0dd6ee32a5c1d83b05d3a2bc141
MoizCodes <km856307@gmail.com>
2024-11-01 19:44:05 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (53.48%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char result = 0;
+        for(char c : s){
+              result^=c;
+            }
+        for(char c : t){
+              result^=c;
+            }
+            return result;
+        
+    }
+};
