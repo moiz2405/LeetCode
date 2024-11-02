569cab7759495542504dbb47ed1d5bfb2f1e89e7
MoizCodes <km856307@gmail.com>
2024-11-03 00:26:06 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (59.78%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.size();
+
+        for (int i = 1;i < n- 1;i++) {
+            if (sentence[i]== ' ') {
+                if (sentence[i - 1]!=sentence[i + 1]) {
+                    return false;
+                }
+            }
+        }
+        if (sentence[0] != sentence[n - 1]) return false;
+        return true;
+    }
+};
